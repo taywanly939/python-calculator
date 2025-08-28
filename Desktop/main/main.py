7777410924020ff3d50609dfd3eb655265f01315
Ali_Yasser <taywanly939@gmail.com>
2025-08-28 22:22:35 +0200

    feat: add complete calclulator logic with err handling

--- Desktop/main/main.py
+++ Desktop/main/main.py
@@ -2,3 +2,18 @@ num1 = float (input("entr a number: "))
 num2 = float (input("entr a number: "))
 opr = input (" :(/, *, -, +)")
 
+if opr == '+':
+    result = num1 + num2
+elif opr == '-':
+    result = num1 - num2
+elif opr == '*':
+    result = num1 * num2
+elif opr == '/':
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error: Division by zero!"
+else:
+    result = "Invalid operation!"
+
+print("Result:", result)
